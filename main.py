f3b0a40e925c4d083e332c990ed1a9465847914d
junesvibes <brianhkent00@icloud.com>
2020-09-14 13:43:41 -0400

    Can't figure out indentions

---
+++ main.py
@@ -0,0 +1,62 @@
+space= "\n"
+print("Welcome to my First Game!")
+
+name= input("What is your name? ")
+age= input("What is your age? ")
+
+
+health = 10
+
+print("You are starting with", health, "heatlh.")
+
+if age >= "18":
+    print("You are old enough to play!")
+    wants_to_play= input("Do you want to play? ").lower()
+        if wants_to_play == "Yes" or "yes":
+        print(space)
+        print("Let's play! ")
+    
+        left_or_right = input("First choice... Left of Right? ")
+        if left_or_right = "left":
+            ans = input("Nice, you follow the path and reach a lake... Do you swim across or go around (across/around)? ")
+
+            if ans== around:
+                print("You went around and reached the other side of the lake")
+
+                elif ans== across:
+                print("You managed to get across, but were bit by a fish and lost 5 health. ")
+                health -= 5
+            
+                else:
+                print("You lost...")
+     else:
+        print("You fell down a lost...")
+    
+    
+    
+    
+    
+    
+    
+else:
+    print("Cya...")
+    elif age >="13":
+        print("You can play with supervision. ")
+    else:
+        print("You are not old enough to play...")
+
+
+
+
+
+
+
+
+
+'''
+string "hello", "hi", "89"
+int 8, 7, -9, 10000
+float 6.0, 7.5, -9.8, -100.0
+bool True, False
+'''
+ 
