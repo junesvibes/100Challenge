c7efcd1abf9f1136543daf1398d47f305385eb73
junesvibes <71022093+junesvibes@users.noreply.github.com>
2020-10-06 12:43:04 -0400

    Add files via upload

---
+++ Big Game/change_later.py
@@ -0,0 +1,20 @@
+space= "\n"
+print("Welcome to the Big Game")
+
+name= input("What is your name? ")
+name= input("How old are you? ")
+
+health = 100 
+
+if age <= "13":
+    wants_to_play == input("We're happy to have you here! Do you want to continue and play?").lower()
+
+    if wants_to_play == "yes":
+        print ("You are starting with", health, "health.")
+        print(space)
+        print ("Amazing, moving on now...")
+
+    
+    
+    
+    if wants_to_play == "no": 
